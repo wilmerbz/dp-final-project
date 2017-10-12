430d0e6ce9421c20982806c014750fc25dbd9d2f
Wilmer BZ <wilmerbz@outlook.com>
2017-10-12 09:57:23 -0500

    Removed project files.

--- SpaceInvaders/core/src/com/space/invaders/interfaces/mensajes/IColega.java
+++
@@ -1,6 +0,0 @@
-package com.space.invaders.interfaces.mensajes;
-
-public interface IColega {
-	void setMediador(IMediador mediador);
-	void recibirMensaje(String mensaje, Object data);
-}
--- SpaceInvaders/core/src/com/space/invaders/interfaces/mensajes/IMediador.java
+++
@@ -1,7 +0,0 @@
-package com.space.invaders.interfaces.mensajes;
-
-public interface IMediador {
-	void registrarColega(IColega colega);
-	void desregistrarColega(IColega colega);
-	void enviarMensaje(IColega sender, String mensaje, Object data);
-}
